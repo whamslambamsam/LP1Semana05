973234e234b571cbd93f52a53f3eb1c628daa38b
Samuel Esteves <ssamuelesteves13@gmail.com>
2025-03-21 17:13:39 +0000

    Added project CarStats.

---
+++ CarStats/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarStats
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
