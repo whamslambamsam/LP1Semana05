bf825ba9a0eb69ac52d9f1b5672aad095cb9beda
Samuel Esteves <ssamuelesteves13@gmail.com>
2025-03-23 03:19:44 +0000

    Add class doc

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -7,6 +7,7 @@ namespace LevelOne
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
+            // I am a C# file. How intriguing! 
         }
     }
 }
