a26903ed8e0ac14114bf591239515a8e2d60a077
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-03-19 15:42:07 +0000

    Add initial code

---
+++ LevelOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LevelOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
