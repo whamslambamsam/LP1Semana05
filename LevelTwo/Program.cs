5e8e2d3331a6ed110eb873823aa0bd9d35f420f4
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-03-19 19:55:26 +0000

    Added LevelTwo project

---
+++ LevelTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LevelTwo
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
