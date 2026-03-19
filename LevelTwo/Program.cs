1f2f43432540539eb52fe203424041bc60e7f913
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-03-19 20:43:48 +0000

    Minor change to variables

--- LevelTwo/Program.cs
+++ LevelTwo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Dynamic;
 using System.Formats.Asn1;
 using System.Reflection;
 using Spectre.Console;
@@ -10,9 +11,9 @@ namespace LevelTwo
     {
         private static void Main(string[] args)
         {
-            var image = new CanvasImage(args[0]);
-            image.MaxWidth(args[1]);
-            
+            var image = new CanvasImage(Convert.ToString(args[0]));
+            image.MaxWidth(Convert.ToInt32(args[1]));
+
             if (args[0] == "" | args[1] == "")
             {
                 image = "tux.jpg";
