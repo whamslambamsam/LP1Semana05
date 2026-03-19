3263adc2ff6daacf62ef0345c176999f206736cc
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-03-19 21:05:36 +0000

    Completed LevelTwo project

--- LevelTwo/Program.cs
+++ LevelTwo/Program.cs
@@ -2,23 +2,26 @@
 using System.ComponentModel;
 using System.Dynamic;
 using System.Formats.Asn1;
+using System.Linq.Expressions;
 using System.Reflection;
+using System.Xml;
 using Spectre.Console;
 
 namespace LevelTwo
 {
-    public class Program
+    public static class Program
     {
         private static void Main(string[] args)
         {
-            var image = new CanvasImage(Convert.ToString(args[0]));
+            var image = new CanvasImage(Convert.ToString($"{args[0]}"));
             image.MaxWidth(Convert.ToInt32(args[1]));
 
             if (args[0] == "" | args[1] == "")
             {
-                image = "tux.jpg";
+                image = new CanvasImage("LP1Semana05\tux.jpg");
                 image.MaxWidth(24);
             }
+
             AnsiConsole.Write(image);
         }
     }
